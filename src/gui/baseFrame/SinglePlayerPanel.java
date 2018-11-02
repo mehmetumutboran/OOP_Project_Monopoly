74eeb99f14892083b63591c46fd5673f4d9e628c
Agabey Alioglu <agabeyalioglu@gmail.com>
2018-11-02 03:01:51 +0300

    username label fix in Single playerPanel

--- src/gui/baseFrame/SinglePlayerPanel.java
+++ src/gui/baseFrame/SinglePlayerPanel.java
@@ -39,7 +39,7 @@ public class SinglePlayerPanel extends JPanel {
         enterButton = new EnterButton("Enter");
         backButton = new BackButton("Back");
         userNameField = new JTextField();
-        userNameLabel = new JLabel();
+        userNameLabel = new JLabel("Username");
 
         userNameField.setBounds((this.width - BUTTON_WIDTH) / 2,
                 (this.height - 4 * BUTTON_HEIGHT) / 2, BUTTON_WIDTH, BUTTON_HEIGHT);
