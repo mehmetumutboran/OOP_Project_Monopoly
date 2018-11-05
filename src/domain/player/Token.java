8794b8807d040f3e10d2b935cfabafce49223beb
Agabey Alioglu <agabeyalioglu@gmail.com>
2018-11-05 21:32:30 +0300

    Removed JSON Simple dependency

--- src/domain/player/Token.java
+++ src/domain/player/Token.java
@@ -1,7 +1,5 @@
 package domain.player;
 
-import org.json.simple.JSONObject;
-
 public class Token {
     private int location;
     private String color;
@@ -35,10 +33,4 @@ public class Token {
         this.color = color;
     }
 
-    public JSONObject toJSON() {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("location", this.location);
-        jsonObject.put("color", this.color);
-        return jsonObject;
-    }
 }
