416b2b5319f27b369afa092e6e47063faa145590
Agabey Alioglu <agabeyalioglu@gmail.com>
2018-11-07 00:16:45 +0300

    Started designing Control display

--- src/gui/controlDisplay/PlayerLabel.java
+++ src/gui/controlDisplay/PlayerLabel.java
@@ -6,9 +6,11 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class PlayerLabel extends JLabel implements MouseListener {
+    private PlayerLabelsPanel playerLabelsPanel;
 
-    public PlayerLabel(String text) {
+    public PlayerLabel(String text, PlayerLabelsPanel panel) {
         super(text);
+        playerLabelsPanel= panel;
         this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
         this.setOpaque(true);
         this.addMouseListener(this);
@@ -16,7 +18,8 @@ public class PlayerLabel extends JLabel implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
-        this.setBackground(this.getBackground().brighter());
+        playerLabelsPanel.getPlayerStatusPanel().setPlayerInfoPanel(this.getText());
+        playerLabelsPanel.getPlayerStatusPanel().getCardLayout().next(playerLabelsPanel.getPlayerStatusPanel());
         System.out.println("Player info clicked");
     }
 
---
+++ src/gui/controlDisplay/PlayerLabelsPanel.java
@@ -0,0 +1,59 @@
+package gui.controlDisplay;
+
+import domain.controller.MonopolyGameController;
+import gui.ColorConverter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class PlayerLabelsPanel extends JLabel {
+    private int INITIAL_X = 0;
+    private int INITIAL_Y = 0;
+    private final int LAST_X = 490;
+    private final int SQUARE_EDGE = 90;
+
+    private ArrayList<PlayerLabel> playerLabels;
+
+    private PlayerStatusPanel playerStatusPanel;
+
+
+    public PlayerLabelsPanel(PlayerStatusPanel playerStatusPanel) {
+        this.playerStatusPanel = playerStatusPanel;
+
+        this.setPreferredSize(playerStatusPanel.getSize());
+
+        initGUI();
+
+        this.setVisible(true);
+
+    }
+
+    private void initGUI() {
+        playerLabels = new ArrayList<>();
+
+        for (int i = 0; i < MonopolyGameController.getInstance().getPlayerListName().size(); i++) {
+            PlayerLabel temp = new PlayerLabel(MonopolyGameController.getInstance().getPlayerListName().get(i), this);
+            temp.setBackground(ColorConverter.getInstance().getColor(
+                    MonopolyGameController.getInstance().getPlayerList().get(i).getToken().getColor()));
+            playerLabels.add(temp);
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int i = 0; i < playerLabels.size(); i++) {
+            if (INITIAL_X + i * SQUARE_EDGE >= LAST_X) {
+                INITIAL_X = (INITIAL_X - 10) % (LAST_X - 10) + 10; //TODO Change
+                INITIAL_Y += INITIAL_Y + SQUARE_EDGE;
+            }
+            playerLabels.get(i).setBounds(INITIAL_X + i * SQUARE_EDGE, INITIAL_Y, SQUARE_EDGE, SQUARE_EDGE);
+            this.add(playerLabels.get(i));
+        }
+    }
+
+    public PlayerStatusPanel getPlayerStatusPanel() {
+        return playerStatusPanel;
+    }
+}
