3829fe445753843d185a04b591aed244bbe03b06
Agabey Alioglu <agabeyalioglu@gmail.com>
2018-11-15 23:36:56 +0300

    Added RandomPlayer logic

--- src/domain/RandomPlayer.java
+++ src/domain/RandomPlayer.java
@@ -1,6 +1,11 @@
 package domain;
 
+import domain.board.Board;
+import domain.board.DeedSquare;
+import domain.board.Square;
 import domain.player.Player;
+
+import java.util.Random;
 
 public class RandomPlayer extends Player {
 
@@ -14,11 +19,43 @@ public class RandomPlayer extends Player {
 
 
     private void playTurn() {
-
         GameLogic.getInstance().roll();
+
+        Square square = Board.getInstance().getSquare(this.getToken().getLocation()[0], this.getToken().getLocation()[1]);
+
+        if(square instanceof DeedSquare){
+            if(((DeedSquare) square).getOwner().equals(this)){
+//                GameLogic.getInstance().payRent();
+            }
+        }
+
+        while(true){
+            if (selectAction()) break;
+        }
 
         GameLogic.getInstance().finishTurn();
 
+    }
+
+    private boolean selectAction() {
+        int x = (new Random()).nextInt(6);
+
+        switch (x){
+//            case 0:
+//                return GameLogic.getInstance().buy();
+//            case 1:
+//                return GameLogic.getInstance().sell();
+//            case 2:
+//                return GameLogic.getInstance().mortgage();
+//            case 3:
+//                return GameLogic.getInstance().unmortgage();
+//            case 4:
+//                return GameLogic.getInstance().upgrade();
+//            case 5:
+//                return GameLogic.getInstance().downgrade();
+            default:
+                return false;
+        }
     }
 
     public synchronized boolean checkTurn() {
