74aa0d91ef3b03db3e1d7aecc714179703a3ba63
mehmettaha <mehmettaha35@gmail.com>
2018-11-16 19:17:42 +0300

    version updated

--- src/gui/baseFrame/BaseFrame.java
+++ src/gui/baseFrame/BaseFrame.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 public class BaseFrame extends JFrame implements Runnable, CloseButtonListener {
     private final int FRAME_WIDTH = 1080;
     private final int FRAME_HEIGHT = 720;
-    private final String CURRENT_VERSION = "v1.0.0";
+    private final String CURRENT_VERSION = "v1.1.0";
 
     private HashMap<String, JPanel> panelMap;
     private static boolean changed = false;
