cc68af84f26c0da570e5ac695a48838b68fcf4cc
Agabey Alioglu <agabeyalioglu@gmail.com>
2018-11-22 04:26:47 +0300

    Implemented RollInterpreter

--- src/domain/interpreter/RollInterpreter.java
+++ src/domain/interpreter/RollInterpreter.java
@@ -1,9 +1,17 @@
 package domain.interpreter;
 
+import domain.GameLogic;
+import domain.MessageConverter;
+import domain.UIUpdater;
+
 public class RollInterpreter implements Interpreter {
     @Override
     public void interpret(String[] message) {
+        String name = message[0];
+        int[] faceValues = MessageConverter.convertStringToIntArray(message[1]);
 
+        GameLogic.getInstance().getPlayer(name).setFaceValues(faceValues);
 
+        UIUpdater.getInstance().setMessage(name + " rolled " + faceValues[0] + " " + faceValues[1] + " " + faceValues[2]); //TODO Mrmonopoly
     }
 }
