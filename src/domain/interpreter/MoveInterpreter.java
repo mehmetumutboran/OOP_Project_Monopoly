adeeb8a5bafb9a5f90ac950c2179a5feb64a4361
Agabey Alioglu <agabeyalioglu@gmail.com>
2018-11-22 04:31:35 +0300

    Implemented MoveInterpreter

--- src/domain/interpreter/MoveInterpreter.java
+++ src/domain/interpreter/MoveInterpreter.java
@@ -1,8 +1,21 @@
 package domain.interpreter;
 
+import domain.GameLogic;
+import domain.MessageConverter;
+import domain.UIUpdater;
+import domain.board.Board;
+import domain.player.Player;
+
 public class MoveInterpreter implements Interpreter {
     @Override
     public void interpret(String[] message) {
+        String name = message[0];
+        int[] location = MessageConverter.convertStringToIntArray(message[1]);
 
+        Player player = GameLogic.getInstance().getPlayer(name);
+
+        player.getToken().setLocation(location);
+
+        UIUpdater.getInstance().setMessage(name + " moved to " + Board.getInstance().getSquare(location[0], location[1]).getName()); //TODO Mrmonopoly
     }
 }
