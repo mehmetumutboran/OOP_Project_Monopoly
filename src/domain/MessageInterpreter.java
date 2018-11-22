33e32ae6647edb9ac81ae98cfd5ff27430d859b4
Agabey Alioglu <agabeyalioglu@gmail.com>
2018-11-22 05:07:29 +0300

    Simplified MessageInterpreter

--- src/domain/MessageInterpreter.java
+++ src/domain/MessageInterpreter.java
@@ -3,7 +3,10 @@ package domain;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import domain.board.*;
+import domain.board.Board;
+import domain.board.DeedSquare;
+import domain.board.Property;
+import domain.board.Railroad;
 import domain.board.specialSquares.Chance;
 import domain.board.specialSquares.CommunityChest;
 import domain.interpreter.*;
@@ -11,10 +14,7 @@ import domain.player.Player;
 import domain.player.Token;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Deque;
 import java.util.HashMap;
-import java.util.LinkedList;
 
 /**
  * Planned as a Class that interprets received Message then updates game state
@@ -56,7 +56,7 @@ public class MessageInterpreter {
     public synchronized void interpret(String m) {
         char flag = m.charAt(0);
 
-        if(interpreterMap.keySet().contains(flag))
+        if (interpreterMap.keySet().contains(flag))
             interpreterMap.get(flag).interpret(m.split("[|]"));
 
         switch (flag) {
@@ -87,12 +87,6 @@ public class MessageInterpreter {
         GameLogic.getInstance().removePlayer(name);
         UIUpdater.getInstance().removeUpdate(name);
     }
-
-//    private void interpretPool(String message) {
-//        GameLogic.getInstance().changePool(message);
-//        UIUpdater.getInstance().setMessage("Pool money is increased by :  " + message + " \nCurrent pool balance is : " + Board.getInstance().getPool());
-//
-//    }
 
 
     private void interpretSpecial(String message) {
@@ -290,194 +284,13 @@ public class MessageInterpreter {
 
     }
 
-    private void interpretMoneyChange(String message) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-        String name;
-        String player = message.substring(message.indexOf('{'));
-        Player received = null;
-        int changedMoney = Integer.parseInt(message.substring(0, message.indexOf('{')));
-        try {
-            received = objectMapper.readValue(player, Player.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assert received != null;
-        name = received.getName();
-        assert received.getName() != null;
-        if (!name.equals(GameLogic.getInstance().getPlayerList().get(0).getName()) && !(received.getReadiness().equals("Bot") && GameLogic.getInstance().getPlayerList().get(0).getReadiness().equals("Host"))) {
-            int money = changedMoney + GameLogic.getInstance().getPlayer(name).getBalance();
-            GameLogic.getInstance().getPlayer(name).setBalance(money);
-        }
-        if (changedMoney > 0) UIUpdater.getInstance().setMessage(name + " 's money increased by " + changedMoney);
-        else UIUpdater.getInstance().setMessage(name + " 's money decreased by " + (-changedMoney));
-    }
-
-    private void interpretMove(String message) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-        int[] location = new int[2];
-        String name = null;
-        try {
-            location = objectMapper.readValue(message, Player.class).getToken().getLocation();
-            name = objectMapper.readValue(message, Player.class).getName();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(name + " in the location " + Arrays.toString(location));
-
-        GameLogic.getInstance().getPlayer(name).getToken().setLocation(location);
-
-        UIUpdater.getInstance().setMessage(name + " moved to " + Board.getInstance().getSquare(location[0], location[1]).getName()); //TODO Mrmonopoly
-    }
-
     private void interpretClose() {
         UIUpdater.getInstance().close();
-    }
-
-    private void interpretQueue(String q) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-        try {
-            String[] players = objectMapper.readValue(q, String[].class);
-//            System.out.println(Arrays.toString(objectMapper.readValue(q, Player[].class)));
-            Deque<String> temp = new LinkedList<>();
-            for (String player : players) {
-                temp.addLast(player);
-            }
-            GameLogic.getInstance().setPlayers(temp);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //Bots play on only host's program
-        if (GameLogic.getInstance().getPlayerList().get(0).getReadiness().equals("Host")) {
-
-            // Check for bots if they starts the game.
-            for (Player player : GameLogic.getInstance().getPlayerList()) {
-                if (player.getReadiness().equals("Bot") && ((RandomPlayer) player).checkTurn()) {
-                    break;
-                }
-            }
-        }
-        UIUpdater.getInstance().turnUpdate();
     }
 
     private void interpretFinishTurn() {
         GameLogic.getInstance().switchTurn();
         UIUpdater.getInstance().turnUpdate();
-    }
-
-    private void interpretRoll(String message) {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-        int[] location = new int[2];
-        String name = null;
-        int[] faceValues = new int[3];
-        try {
-            location = objectMapper.readValue(message, Player.class).getToken().getLocation();
-            name = objectMapper.readValue(message, Player.class).getName();
-            faceValues = objectMapper.readValue(message, Player.class).getFaceValues();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-//        GameLogic.getInstance().getPlayer(name).getToken().setLocation(location);
-
-        UIUpdater.getInstance().setMessage(name + " rolled " + faceValues[0] + " " + faceValues[1] + " " + faceValues[2]); //TODO Mrmonopoly
-    }
-
-
-    private void interpretBuy(String message) {
-        /*may be wrong !!!! */
-        System.out.println("in interpret buy flag");
-
-        ObjectMapper objectMapper2 = new ObjectMapper();
-        objectMapper2.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-
-        int[] location;
-        String name = null;
-        Square square = null;
-        try {
-
-            name = objectMapper2.readValue(message, Player.class).getName();
-            System.out.println("name");
-
-            location = objectMapper2.readValue(message, Player.class).getToken().getLocation();
-            /* sq will be done for other classes */
-            System.out.println("location");
-            square = Board.getInstance().getSquare(location[0], location[1]);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (square instanceof SpecialSquareStrategy) return;
-        if (square == null) return;
-
-        GameLogic.getInstance().getPlayer(name).decreaseMoney(((DeedSquare) square).getBuyValue());
-
-        if (square instanceof Property) {
-            GameLogic.getInstance().getPlayer(name).getOwnedProperties().add(((Property) square));
-            UIUpdater.getInstance().setMessage(name + " bought " + square); //TODO Mrmonopoly
-            ((Property) square).setOwner(GameLogic.getInstance().getPlayer(name));
-
-
-        } else if (square instanceof Railroad) {
-            GameLogic.getInstance().getPlayer(name).getOwnedRailroads().add(((Railroad) square));
-            UIUpdater.getInstance().setMessage(name + " bought " + square); //TODO Mrmonopoly
-            ((Railroad) square).setOwner(GameLogic.getInstance().getPlayer(name));
-
-
-        } else if (square instanceof Utility) {
-            GameLogic.getInstance().getPlayer(name).getOwnedUtilities().add(((Utility) square));
-            UIUpdater.getInstance().setMessage(name + " bought " + square); //TODO Mrmonopoly
-            ((Utility) square).setOwner(GameLogic.getInstance().getPlayer(name));
-        }
-
-
-    }
-
-
-    private void interpretRent(String message) {
-        System.out.println("in interpretRent");
-
-        ObjectMapper objectMapper3 = new ObjectMapper();
-        objectMapper3.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-
-        int[] location;
-        String name = null;
-        Square square = null;
-        int rentVal = 0;
-        try {
-            name = objectMapper3.readValue(message, Player.class).getName();
-            location = objectMapper3.readValue(message, Player.class).getToken().getLocation();
-            square = Board.getInstance().getSquare(location[0], location[1]);
-            rentVal = ((DeedSquare) square).getRent();
-            /* sq will be done for other classes */
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        /*payer*/
-        if (square instanceof SpecialSquareStrategy) return;
-        if (square == null) return;
-        GameLogic.getInstance().getPlayer(name).decreaseMoney(rentVal);
-        /*taker*/
-
-        if (square instanceof Property) {
-            GameLogic.getInstance().getPlayer(((Property) square).getOwner().getName()).increaseMoney(rentVal);
-            UIUpdater.getInstance().setMessage(name + " paid payRent " + rentVal + " dollars to " + ((DeedSquare) square).getOwner().getName()); //TODO Mrmonopoly
-
-        } else if (square instanceof Railroad) {
-            GameLogic.getInstance().getPlayer(((Railroad) square).getOwner().getName()).increaseMoney(rentVal);
-            UIUpdater.getInstance().setMessage(name + " paid payRent " + rentVal + " dollars to " + ((DeedSquare) square).getOwner().getName()); //TODO Mrmonopoly
-
-        } else if (square instanceof Utility) {
-            GameLogic.getInstance().getPlayer(((Utility) square).getOwner().getName()).increaseMoney(rentVal);
-            UIUpdater.getInstance().setMessage(name + " paid payRent " + rentVal + " dollars to " + ((DeedSquare) square).getOwner().getName()); //TODO Mrmonopoly
-        }
-
-
     }
 
 
