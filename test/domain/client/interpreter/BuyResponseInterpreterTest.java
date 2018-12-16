13c51365aad46af75a2049cd4aebff404940b4f3
Agabey Alioglu <agabeyalioglu@gmail.com>
2018-12-16 17:14:44 +0300

    Buy Response Bugfix

--- test/domain/client/interpreter/BuyResponseInterpreterTest.java
+++ test/domain/client/interpreter/BuyResponseInterpreterTest.java
@@ -1,14 +1,16 @@
 package domain.client.interpreter;
 
+import domain.client.PlayerActionController;
 import domain.server.board.Board;
 import domain.server.board.DeedSquare;
-import domain.server.board.Property;
 import domain.server.player.Player;
 import domain.util.GameInfo;
+import network.server.serverFacade.ServerFacade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BuyResponseInterpreterTest {
 
@@ -16,20 +18,25 @@ class BuyResponseInterpreterTest {
 
     @BeforeEach
     void setUp() {
+        if (ServerFacade.getInstance().getServer() != null)
+            ServerFacade.getInstance().shutDown();
         GameInfo.getInstance().reset();
-        buyer = new Player("Buyer");
-        GameInfo.getInstance().addPlayer(buyer);
+        PlayerActionController.getInstance().host("Buyer", 2222, false);
+        while (GameInfo.getInstance().getPlayer("Buyer") == null) {
+            continue;
+        }
+        buyer = GameInfo.getInstance().getPlayer("Buyer");
     }
 
-    //@Test
+    @Test
     void interpret() {
         String squareName = "Ventura Boulevard";
-        int buyValue = ((DeedSquare)Board.getInstance().getNameGivenSquare(squareName)).getBuyValue();
-        String [] message = {"Flag", buyer.getName(), "" + (buyer.getBalance() - buyValue), squareName};
+        int buyValue = ((DeedSquare) Board.getInstance().getNameGivenSquare(squareName)).getBuyValue();
+        String[] message = {"Flag", buyer.getName(), "" + (buyer.getBalance() - buyValue), squareName};
         new BuyResponseInterpreter().interpret(message);
-        assertEquals(3200-480,GameInfo.getInstance().getPlayer("Buyer").getBalance());
-        assertTrue(((DeedSquare)Board.getInstance().getNameGivenSquare(squareName)).isOwned());
-        assertEquals(buyer, GameInfo.getInstance().getPlayer(((DeedSquare)Board.getInstance().getNameGivenSquare(squareName)).getOwner()));
+        assertEquals(3200 - 480, GameInfo.getInstance().getPlayer("Buyer").getBalance());
+        assertTrue(((DeedSquare) Board.getInstance().getNameGivenSquare(squareName)).isOwned());
+        assertEquals(buyer, GameInfo.getInstance().getPlayer(((DeedSquare) Board.getInstance().getNameGivenSquare(squareName)).getOwner()));
         assertTrue(GameInfo.getInstance().getPlayer("Buyer").getOwnedProperties().contains(Board.getInstance().getNameGivenSquare(squareName)));
     }
 }
