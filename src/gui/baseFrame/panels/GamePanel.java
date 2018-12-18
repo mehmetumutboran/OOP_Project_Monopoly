872333ae6dc28b7db8b9a9b4587fcab79ed381f1
Mehmet Umut Boran <mboran16@ku.edu.tr>
2018-12-18 21:07:10 +0300

    Fix

--- src/gui/baseFrame/panels/GamePanel.java
+++ src/gui/baseFrame/panels/GamePanel.java
@@ -190,132 +190,6 @@ public class GamePanel extends JPanel implements GameStartedListener, TokenMovem
 //
    }
 
-    public int[] indexToCoor(int label, int index, String name) {
-        int xLoc = 0;
-        int yLoc = 0;
-        int pIndex = 0;
-        int widthZero = this.getWidth();
-        int heightZero = this.getHeight();
-        int widthOne = 4 * widthZero / 5;
-        int heightOne = 3 * heightZero / 4;
-        int widthTwo = widthZero / 2;
-        int heightTwo = heightZero / 2;
-        for (int i = 0; i < tokenlist.size(); i++) {
-            if (tokenlist.get(i).getOwner().equals(name)) {
-                pIndex = i;
-                break;
-            }
-        }
-
-        if (label == 0) {
-            if (index >= 0 && index < 14) {
-                if (pIndex > 6) {
-                    yLoc = heightZero * 9 / 100 + index * heightZero / 17 + 15; //59
-                    xLoc = widthZero - 2 * widthZero / 17 + (pIndex - 6) * 15;
-                } else {
-                    yLoc = heightZero * 9 / 100 + index * heightZero / 17;
-                    xLoc = widthZero - 2 * widthZero / 17 + pIndex * 15;
-                }
-            } else if (index >= 14 && index < 28) {
-                if (pIndex > 6) {
-                    yLoc = 905 + (pIndex - 6) * 15;
-                    xLoc = (1240 - (index - 14) * 89) + 44 + 15; //14
-                } else {
-                    yLoc = 905 + pIndex * 15;
-                    xLoc = (1240 - (index - 14) * 89) + 44;
-                }//14
-            } else if (index >= 28 && index < 42) {
-                if (pIndex > 6) {
-                    yLoc = 905 - (index - 28) * 59 + 15;
-                    xLoc = 30 + (pIndex - 6) * 15;
-                } else {
-                    yLoc = 905 - (index - 28) * 59;
-                    xLoc = 30 + pIndex * 15;
-                }
-            } else if (index >= 42 && index < 56) {
-                if (pIndex > 6) {
-                    yLoc = 90 - (pIndex - 6) * 15;
-                    xLoc = (30 + (index - 42) * 89) + 44 + 15; //42
-                } else {
-                    yLoc = 90 - pIndex * 15;
-                    xLoc = (30 + (index - 42) * 89) + 44; //42
-                }
-            }
-        } else if (label == 1) {
-            if (index >= 0 && index < 10) {
-                if (pIndex > 6) {
-                    yLoc = 210 + index * heightOne / 13 + 15; //heightZero*11/100 + heightOne*9/130
-                    xLoc = widthZero - 2 * (widthOne / 13 + widthZero / 17) + (pIndex - 6) * 15;
-                } else {
-                    yLoc = heightZero * 9 / 100 + heightOne / 13 + index * heightOne / 13;
-                    xLoc = widthZero - 2 * (widthOne / 13 + widthZero / 17) + pIndex * 15;
-                }
-            } else if (index >= 10 && index < 20) {
-                if (pIndex > 6) {
-                    yLoc = 790 + (pIndex - 6) * 15;
-                    xLoc = (1070 - (index - 10) * 78) + 39 + 15; //10
-                } else {
-                    yLoc = 790 + +pIndex * 15;
-                    xLoc = (1070 - (index - 10) * 78) + 39; //10
-                }
-            } else if (index >= 20 && index < 30) {
-                if (pIndex > 6) {
-                    yLoc = 790 - (index - 20) * 57 + 15;
-                    xLoc = 200 + (pIndex - 6) * 15;
-                } else {
-                    yLoc = 790 - (index - 20) * 57;
-                    xLoc = 200 + pIndex * 15;
-                }
-            } else if (index >= 30 && index < 40) {
-                if (pIndex > 6) {
-                    yLoc = 210 - (pIndex - 6) * 15;
-                    xLoc = (200 + (index - 30) * 78) + 39 + 15; //30
-                } else {
-                    yLoc = 210 - pIndex * 15;
-                    xLoc = (200 + (index - 30) * 78) + 39; //30
-                }
-            }
-        } else if (label == 2) {
-            if (index >= 0 && index < 6) {
-                if (pIndex > 6) {
-                    yLoc = 325 + index * 58 + 15;
-                    xLoc = 905 + (pIndex - 6) * 15;
-                } else {
-                    yLoc = 325 + index * 58;
-                    xLoc = 905 + pIndex * 15;
-                }
-            } else if (index >= 6 && index < 12) {
-                if (pIndex > 6) {
-                    yLoc = 675 + (pIndex - 6) * 15;
-                    xLoc = (905 - (index - 6) * 75) + 37 + 15; //6
-                } else {
-                    yLoc = 675 + pIndex * 15;
-                    xLoc = (905 - (index - 6) * 75) + 37; //6
-                }
-            } else if (index >= 12 && index < 18) {
-                if (pIndex > 6) {
-                    yLoc = 675 - (index - 12) * 58 + 15;
-                    xLoc = 370 + (pIndex - 6) * 15;
-                } else {
-                    yLoc = 675 - (index - 12) * 58;
-                    xLoc = 370 + pIndex * 15;
-                }
-            } else if (index >= 18 && index < 24) {
-                if (pIndex > 6) {
-                    yLoc = 325 - (pIndex - 6) * 15;
-                    xLoc = (370 + (index - 18) * 75) + 37 + 15; //18
-                } else {
-                    yLoc = 325 - pIndex * 15;
-                    xLoc = (370 + (index - 18) * 75) + 37; //18
-                }
-            }
-        }
-        int[] coor = new int[2];
-        coor[0] = xLoc;
-        coor[1] = yLoc;
-        return coor;
-    }
-
     @Override
     public void onDiceRolledEvent(int [] faces) {
         for (int i = 0; i < diceList.size(); i++) {
@@ -333,6 +207,8 @@ public class GamePanel extends JPanel implements GameStartedListener, TokenMovem
             tl.setOpaque(false);
             this.add(tl);
             tokenlist.add(tl);
+            this.revalidate();
+            this.repaint();
         }
     }
 }
