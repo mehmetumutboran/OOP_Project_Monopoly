3bf9f8204448b14327eb641125b8ba973d3c8e70
Mehmet Umut Boran <mboran16@ku.edu.tr>
2019-01-07 02:25:46 +0300

    Load Token

--- src/domain/client/interpreter/LoadResponseInterpreter.java
+++ src/domain/client/interpreter/LoadResponseInterpreter.java
@@ -2,7 +2,10 @@ package domain.client.interpreter;
 
 import domain.client.ClientCommunicationHandler;
 import domain.client.UIUpdater;
+import domain.util.GameInfo;
 import domain.util.LoadGameHandler;
+
+import java.util.ArrayList;
 
 public class LoadResponseInterpreter implements ResponseInterpretable {
     @Override
@@ -11,6 +14,7 @@ public class LoadResponseInterpreter implements ResponseInterpretable {
         LoadGameHandler.getInstance().loadGame(load);
         UIUpdater.getInstance().changePanel("Game");
         UIUpdater.getInstance().startGame();
+        UIUpdater.getInstance().startTokens(new ArrayList<>(GameInfo.getInstance().getPlayerQueue()));
         ClientCommunicationHandler.getInstance().sendReceived();
 
     }
