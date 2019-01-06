0504d617d47d7d4dd1a8b8d570d55ba554c9d20a
Agabey Alioglu <agabeyalioglu@gmail.com>
2019-01-07 02:22:00 +0300

    set Started in load

--- src/domain/client/interpreter/LoadResponseInterpreter.java
+++ src/domain/client/interpreter/LoadResponseInterpreter.java
@@ -2,6 +2,7 @@ package domain.client.interpreter;
 
 import domain.client.ClientCommunicationHandler;
 import domain.client.UIUpdater;
+import domain.util.GameInfo;
 import domain.util.LoadGameHandler;
 
 public class LoadResponseInterpreter implements ResponseInterpretable {
@@ -9,6 +10,7 @@ public class LoadResponseInterpreter implements ResponseInterpretable {
     public void interpret(String[] message) {
         String load = message[1];
         LoadGameHandler.getInstance().loadGame(load);
+        GameInfo.getInstance().startGame();
         UIUpdater.getInstance().changePanel("Game");
         UIUpdater.getInstance().startGame();
         ClientCommunicationHandler.getInstance().sendReceived();
